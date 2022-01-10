3dd533620ccbcdc23306401593c9931fc9e596ab
April W <april.weaver@revature.net>
2022-01-10 14:03:24 -0500

    made the main classes

---
+++ src/Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("\t\tWelcome to today's class! \nToday we will be checking if a bishop can reach a " +
+                "certain \nspot on the board in the given amount of moves.");
+        //Bishop bishop = new Bishop("","",0);
+    }
+}
